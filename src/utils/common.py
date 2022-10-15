6ad8a6235f22927b9a64499cde7c8cb81718d802
Sarad Mishra <saradmishra28@gmail.com>
2022-10-15 09:42:42 +0530

    updated for moving files

--- src/utils/common.py
+++ src/utils/common.py
@@ -1,5 +1,6 @@
 import imghdr
 import os
+import shutil
 from tkinter import Image
 import yaml
 import logging
@@ -25,21 +26,37 @@ def save_json(path: str, data: dict) -> None:
 
     logging.info(f"json file saved at: {path}")
 
-def validate_img(img_full_path):
-    try:
-        logging.info(f"Started verify img: {img_full_path}")
-        img = Image.open(img_full_path)
-        img.verify()
-        if len(img.getbands()) !=3 or imghdr.what(img_full_path) not in ['jpeg','png']:
-            
+# def validate_img(img_full_path,bad_img_dir):
+#     try:
+#         logging.info(f"Started verify img: {img_full_path}")
+#         img = Image.open(img_full_path)
+#         img.verify()
+#         if len(img.getbands()) !=3 or imghdr.what(img_full_path) not in ['jpeg','png']:
+#             bad_data_path = os.path.join(bad_img_dir, img)
+#             shutil.move(img_full_path, bad_data_path)
+#             continue
 
-        logging.info(f"Completed verify img: {img_full_path}")
-        return True
-    except Exception as e:
+#         logging.info(f"Completed verify img: {img_full_path}")
+#         return True
+#     except Exception as e:
+#         logging.exception(e)
+#         raise e
 
 
 def moving_data(source,destination):
-    for imgs in os.listdir(source):
-        if validate_img(os.path.join(source,imgs)): 
+    logging.info(f"Images are moving to local directory")
+    try:
+        for imgs in os.listdir(source):
+            logging.info(f"image name{imgs} & source name {source}")
+            # img_source = os.path.join(source,imgs)
+            img_source = source +'\\' +imgs
+
+            img_destin = destination[0] +'\\' +imgs
+            logging.info(f"{img_source} ******* {img_destin}")
+            shutil.move(img_source,img_destin) 
+            logging.info(f"{img_source} is moved to local directory")
+    except Exception as e:
+        logging.exception(e)
+        raise e
 
 
